9f9d2fead6887f88c2b6952f4644297da56bffdf
Soton <soton371@gmail.com>
2023-12-17 22:18:31 +0600

    update and fetch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 from models import Intro, IntroCrude
 from mongoengine import connect
@@ -14,12 +15,60 @@ async def read_root():
 # start for intro
 @app.post('/add_intro')
 async def add_intro(new_intro: IntroCrude):
-    intro = Intro(
-        introOfName=new_intro.introOfName,
-        name=new_intro.name,
-        whoAreYou=new_intro.whoAreYou,
-        shortBio=new_intro.shortBio
-    )
-    intro.save()
-    return {'message': 'Intro add successfully'}
+    try:
+        intro = Intro(
+            introOfName=new_intro.introOfName,
+            name=new_intro.name,
+            whoAreYou=new_intro.whoAreYou,
+            shortBio=new_intro.shortBio
+        )
+        intro.save()
+        return {
+            'status': True,
+            'message': 'Intro add successfully'}
+    except Exception as e:
+        print(f'Failed to add intro: {e}')
+        return {
+            'status': True,
+            'message': 'Intro add successfully'}
+
+
+@app.get('/get_intro')
+async def get_intro():
+    try:
+        intro_json = json.loads(Intro.objects.first().to_json())
+        return {
+            "status": True,
+            "message": "Fetch intro successfully",
+            "data": intro_json}
+    except Exception as e:
+        print(f"Failed to fetch into e: {e}")
+        return {
+            "status": False,
+            "message": "Failed to fetch intro",
+            "data": None}
+
+
+@app.patch("/update_intro")
+async def update_intro(intro_data: IntroCrude):
+    try:
+        intro = Intro.objects.first()
+        intro.update(
+            introOfName=intro_data.introOfName,
+            name=intro_data.name,
+            whoAreYou=intro_data.whoAreYou,
+            shortBio=intro_data.shortBio,
+        )
+        intro.save()
+        return {
+            "status": True,
+            "message": "Intro updated successfully",
+        }
+    except Exception as e:
+        print(f"Failed to update intro: {e}")
+        return {
+            "status": False,
+            "message": "Failed to update intro",
+        }
+
 # end for intro
